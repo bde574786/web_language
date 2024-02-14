50af9ed7efe7d0c7ff1d14074191016a8a679a1b
bde574786 <jungminsu07150@gmail.com>
2024-02-14 11:54:18 +0900

    Todo Javascript에서 Flask로 폼 제출

---
+++ html/login/static/login.js
@@ -0,0 +1,14 @@
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    
+    // form 제출 방지
+    e.preventDefault();
+
+    /*
+    FormData 객체를 사용하여 폼 데이터를 수집
+    this는 현재 실행 중인 코드 컨텍스트 내의 객체를 참조(여기서는 이벤트리스너가 연결된 폼 요소를 가르키게 됨)
+    */
+    var formData = new FormData(this)
+
+    fetch('http://localhost:5000/')
+
+})
