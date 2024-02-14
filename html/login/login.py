fc8148d4c5e90a79e8e4571d69b907b3fea64b3a
bde574786 <jungminsu07150@gmail.com>
2024-02-14 12:36:55 +0900

    fetch API로 FormData 전송

--- html/login/login.py
+++ html/login/login.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -10,5 +10,10 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/login/post', methods=['POST'])
+def login_post():
+    print(request.form)
+    return 'Form data received'
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
