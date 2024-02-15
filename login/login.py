88d67e9560829b832235d75ecc41a02cc95ed680
bde574786 <jungminsu07150@gmail.com>
2024-02-15 11:18:01 +0900

    Flask와 MySQL 연결, 사용자 존재 여부 확인

--- login/login.py
+++ login/login.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# MySQL 설정
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'asd123'
+app.config['MYSQL_DB'] = 'web_db'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -13,7 +22,21 @@ def login():
 @app.route('/login/post', methods=['POST'])
 def login_post():
     data = request.json
-    return jsonify(data)
+    
+    id = data['id']
+    password = data['password']
+
+    query = f"SELECT * FROM user WHERE id='{id}' AND password='{password}'"
+
+    cursor = mysql.connection.cursor()
+    cursor.execute(query)
+    
+    result = cursor.fetchall()
+    
+    if result:
+        return jsonify(data)
+    return jsonify({})
+        
     
 if __name__ == '__main__':
     app.run(debug=True)
